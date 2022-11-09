b182561cad220f713777576ddfff2de7b303ea18
Алиса <lissa.faul@mail.ru>
2022-11-09 16:32:41 +0300

    removed superfluous from class CustomerType

--- src/CustomerType.java
+++ src/CustomerType.java
@@ -1,13 +1,4 @@
 public enum CustomerType {
-    HUMAN("human"),
-    BUSINESS("business");
-
-    private final String type;
-    CustomerType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
+    HUMAN,
+    BUSINESS
 }
